f3655d784de864ecb9afab2e01268e3c914f2232
Dragma <musaevilkin29@gmail.com>
2020-03-29 11:00:38 +0400

    Add config for heroku

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,21 @@ class App {
     private appConfig: AppConfig;
     private appServer: AppServer;
     constructor() {
-        this.appConfig = YAML.parse(readFileSync(".env.yml").toString());
+        if(process.env.ENV == "production")
+        {
+            this.appConfig = {
+                server: {
+                    port: Number.parseInt(process.env.PORT)
+                },
+                redis: {
+                    url: process.env.REDIS
+                }
+            };
+        }
+        else
+        {
+            this.appConfig = YAML.parse(readFileSync(".env.yml").toString());
+        }
         this.appServer = new AppServer(this.appConfig);
     }
 
